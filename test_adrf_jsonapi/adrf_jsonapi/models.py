58214385b05464e0f6fa54a66df8e470414fe67c
imgvoid <imgvoid@gmail.com>
2023-07-16 21:44:10 +0300

    tests

--- test_adrf_jsonapi/adrf_jsonapi/models.py
+++ test_adrf_jsonapi/adrf_jsonapi/models.py
@@ -4,13 +4,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 class TestIncludedRelation(models.Model):
-    text_included_relation = models.CharField(max_length=128)
-    int_included_relation = models.IntegerField()
-    bool_included_relation = models.BooleanField()
-    choice_int_included_relation = models.IntegerField(choices=((1, 'One'), (2, 'Two')))
+    text_included_relation = models.CharField(max_length=128, default='')
+    int_included_relation = models.IntegerField(default=1)
+    bool_included_relation = models.BooleanField(default=False)
+    choice_int_included_relation = models.IntegerField(choices=((1, 'One'), (2, 'Two')), default=1)
     choice_str_included_relation = models.CharField(max_length=9, choices=(
         ('UK', 'United Kingdom'), ('US', 'United States')
-    ))
+    ), default='UK')
     array_included_relation = ArrayField(models.IntegerField(), size=2, default=list)
     
     class Meta:
@@ -22,13 +22,13 @@ class TestIncludedRelation(models.Model):
 
 
 class TestIncluded(models.Model):
-    text_included = models.CharField(max_length=128)
-    int_included = models.IntegerField()
-    bool_included = models.BooleanField()
-    choice_int_included = models.IntegerField(choices=((1, 'One'), (2, 'Two')))
+    text_included = models.CharField(max_length=128, default='')
+    int_included = models.IntegerField(default=1)
+    bool_included = models.BooleanField(default=False)
+    choice_int_included = models.IntegerField(choices=((1, 'One'), (2, 'Two')), default=1)
     choice_str_included = models.CharField(max_length=9, choices=(
         ('UK', 'United Kingdom'), ('US', 'United States')
-    ))
+    ), default='UK')
     array_included = ArrayField(models.IntegerField(), size=2, default=list)
     foreign_key_included = models.ForeignKey(TestIncludedRelation, on_delete=models.SET_NULL, null=True, blank=True)
     many_to_many_included = models.ManyToManyField(TestIncludedRelation, blank=True, related_name='test_included_relation_many')
@@ -42,13 +42,13 @@ class TestIncluded(models.Model):
 
 # TODO: create the MultipleChoiceField from the module or the ArrayField
 class Test(models.Model):
-    text = models.CharField(max_length=128)
-    int = models.IntegerField()
-    bool = models.BooleanField()
-    choice_int = models.IntegerField(choices=((1, 'One'), (2, 'Two')))
+    text = models.CharField(max_length=128, default='')
+    int = models.IntegerField(default=1)
+    bool = models.BooleanField(default=False)
+    choice_int = models.IntegerField(choices=((1, 'One'), (2, 'Two')), default=1)
     choice_str = models.CharField(max_length=9, choices=(
         ('UK', 'United Kingdom'), ('US', 'United States')
-    ))
+    ), default='UK')
     array = ArrayField(models.IntegerField(), size=2, default=list)
     foreign_key = models.ForeignKey(TestIncluded, on_delete=models.SET_NULL, null=True, blank=True)
     many_to_many = models.ManyToManyField(TestIncluded, blank=True, related_name='test_included_many')
--- test_adrf_jsonapi/jsonapi/tests.py
+++ test_adrf_jsonapi/jsonapi/tests.py
@@ -1,89 +1,59 @@
-import random
-import string
 from django.test import TestCase
 from adrf_jsonapi.models import Test, TestIncluded, TestIncludedRelation
 from jsonapi.model_serializers import JSONAPIModelSerializer
 
+
 class TestModelSerializer(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.test_included_relation_fields = tuple(filter(
-            lambda x: not x.startswith('_') and x != 'id', list([
-                TestIncludedRelation.objects.create(
-                    **cls.test_included_relation_data()
-                ) for _ in range(10)
-            ][0].__dict__.keys())
-        ))
-        cls.test_included_relation_query = TestIncludedRelation.objects.all()
+        test_data = cls.get_data_map(Test)
+        [Test.objects.create(**test_data) for _ in range(10)]
+        cls.test_included_relation_query = Test.objects.all()
     
     @staticmethod
-    def get_test_included_relation_serializer():
-        class TestIncludedSerializer(JSONAPIModelSerializer):
+    def get_test_serializer():
+        class TestSerializer(JSONAPIModelSerializer):
             class Meta:
-                model, model_type = TestIncludedRelation, 'test-included-relation'
+                model, model_type = Test, 'test'
                 fields = ['__all__']
-        
-        return TestIncludedSerializer
+        return TestSerializer
     
-    @staticmethod
-    def test_included_relation_data():
-        return {
-            'text_included_relation': ''.join(
-                random.choice(string.ascii_lowercase + string.digits) for _ in range(
-                    TestIncludedRelation.text_included_relation.field.max_length
-                )
-            ),
-            'int_included_relation': random.choice(range(256)),
-            'bool_included_relation': random.choice((True, False)),
-            'choice_int_included_relation': random.choice([
-                choice[0] for choice in 
-                TestIncludedRelation.choice_int_included_relation.field.choices
-            ]),
-            'choice_str_included_relation': random.choice([
-                choice[0] for choice in 
-                TestIncludedRelation.choice_str_included_relation.field.choices
-            ]),
-            'array_included_relation': [
-                random.choice(range(0, 256)) for _ in 
-                range(TestIncludedRelation.array_included_relation.field.size)
-            ]
-        }
+    @classmethod
+    def get_data_map(cls, model):
+        fields, forward_relations = {}, {}
+        for field in model._meta.fields:
+            data = fields if not field.remote_field else forward_relations
+            data[field.name] = field
+        cls.test_fields = fields = {key: fields[key] for key in list(filter(
+            lambda x: x != 'id', fields.keys()
+        ))}
+        data = {key: fields[key].default for key in fields}
+        data = {key: val() if type(val) == type else val for key, val in data.items()}
+        return data
 
     async def test_list(self):
-        list_serializer = self.get_test_included_relation_serializer()
+        list_serializer = self.get_test_serializer()
         model_type = list_serializer.Meta.model_type
-        fields = self.test_included_relation_fields
-        list_serializer = list_serializer(self.test_included_relation_query, many=True)
-        data = await list_serializer.data
+        fields = self.test_fields
+        data = await list_serializer(
+            self.test_included_relation_query, many=True
+        ).data
         data = data.get('data')
-        assert_equal = (
-            *((obj.get('type'), model_type) for obj in data),
-        )
-        assert_type = (
-            (data, list),
-            *((obj.get('attributes'), dict) for obj in data),
-        )
-        #assert_in = (
-        #    (obj.get(field), ) for obj in data for field in fields
-        #)
-        assert_dict_contains_subset = (
-            *(self.assertIn(field, obj.get('attributes').keys()) for obj in data for field in fields),
-        )
-        for assertion in assert_equal:
-            self.assertEqual(*assertion)
-        for assertion in assert_type:
-            self.assertIsInstance(*assertion)
+        self.assertIsInstance(data, list)
+        [self.assertEqual(obj.get('type'), model_type) for obj in data]
+        [self.assertIsInstance(obj.get('attributes'), dict) for obj in data]
+        [self.assertIn(field, obj.get('attributes').keys()) 
+         for obj in data for field in fields]
         
     async def test_get(self):
-        list_serializer = self.get_test_included_relation_serializer()
+        list_serializer = self.get_test_serializer()
         model_type = list_serializer.Meta.model_type
-        list_serializer = list_serializer(await self.test_included_relation_query.afirst())
-        data = await list_serializer.data
+        fields = self.test_fields
+        data = await list_serializer(
+            await self.test_included_relation_query.afirst()
+        ).data
         data = data.get('data')
-        assert_equal = (
-            (type(data), dict),
-            (data['type'], model_type),
-            (type(data.get('attributes')), dict)
-        )
-        for assertion in assert_equal:
-            self.assertEqual(*assertion)
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['type'], model_type)
+        self.assertIsInstance(data.get('attributes'), dict)
+        [self.assertIn(field, data.get('attributes').keys()) for field in fields]
